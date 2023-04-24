d38db6f4f1d83407466171351737eb614dc2a259
Evgeny Nerush <eugene.nerush@gmail.com>
2023-04-24 13:49:31 +0300

    bugfix in performance_test; add vector functions to core

--- performance-test.zig
+++ performance-test.zig
@@ -4,54 +4,68 @@
 //
 // zig run performance-test.zig -O ReleaseFast
 //
-// The rotation of a big number of particles in a constant magnetic field is simulated.
-// The test should print "8.0..." as a result.
-// On Xeon(R) X5550 the test takes about 110 seconds.
+// The test should print  as a result "x = 1." (or 0.9...) and "px = 10.00..." (or "9.99...").
+// On Xeon(R) X5550 the test takes about 110 seconds for Vay's pusher.
 
 const std = @import("std");
 const Vec3 = @import("src/core.zig").Vec3;
-const vay3P = @import("src/vay.zig").simdVay3P;
-const midpoint3R = @import("src/midpointR.zig").simdMidpoint3R;
+const pusher3P = @import("src/vay.zig").vay3P; // place here the momentum pusher you want to test
+const pusher3R = @import("src/midpointR.zig").midpoint3R; // place here the coordinate pusher you want to test
 const print = std.debug.print;
 
 const Particle = struct { x: f64, y: f64, z: f64, px: f64, py: f64, pz: f64 };
 
+// The rotation of a big number of particles in a constant magnetic field is simulated. The average
+// coordinate and momentum after even number of periods is computed.
 const p_initial = Vec3{ .x = 10, .y = 0, .z = 0 }; // initial momentum
 const gamma_initial: f64 = std.math.sqrt(1 + p_initial.x * p_initial.x + p_initial.y * p_initial.y + p_initial.z * p_initial.z);
 const charge_mass_ratio: f64 = -1.0; // electrons
 const magnetic_field = Vec3{ .x = 0, .y = 0, .z = 10 };
 const electric_field = Vec3{ .x = 0, .y = 0, .z = 0 };
-const period: f64 = 2 * std.math.pi * std.math.sqrt(magnetic_field.x * magnetic_field.x +
+const period: f64 = 2 * std.math.pi * gamma_initial /
+    std.math.sqrt(magnetic_field.x * magnetic_field.x +
     magnetic_field.y * magnetic_field.y +
-    magnetic_field.z * magnetic_field.z) / gamma_initial;
+    magnetic_field.z * magnetic_field.z);
 const time: f64 = 10 * period;
 const n_time_steps: i32 = 1000;
 const dt: f64 = time / @intToFloat(f64, n_time_steps);
 
+const n_particles: usize = 1_000_000;
+
 fn step(particle: Particle) Particle {
     const p0 = Vec3{ .x = particle.px, .y = particle.py, .z = particle.pz };
-    const p = vay3P(p0, charge_mass_ratio, dt, electric_field, magnetic_field);
+    const p = pusher3P(p0, charge_mass_ratio, dt, electric_field, magnetic_field);
 
     const r0 = Vec3{ .x = particle.x, .y = particle.y, .z = particle.z };
-    const gamma: f64 = std.math.sqrt(1 + p.x * p.x + p.y * p.y + p.y * p.y);
+    const gamma: f64 = std.math.sqrt(1 + p.x * p.x + p.y * p.y + p.z * p.z);
     const v = Vec3{ .x = p.x / gamma, .y = p.y / gamma, .z = p.z / gamma };
-    const r = midpoint3R(r0, v, dt);
+    const r = pusher3R(r0, v, dt);
     return Particle{ .x = r.x, .y = r.y, .z = r.z, .px = p.x, .py = p.y, .pz = p.z };
+}
+
+fn halfStepR(timestep_sign: f64, particle: Particle) Particle {
+    const r0 = Vec3{ .x = particle.x, .y = particle.y, .z = particle.z };
+    const gamma: f64 = std.math.sqrt(1 + particle.px * particle.px + particle.py * particle.py + particle.pz * particle.pz);
+    const v = Vec3{ .x = particle.px / gamma, .y = particle.py / gamma, .z = particle.pz / gamma };
+    const r = pusher3R(r0, v, dt / 2 * timestep_sign);
+    return Particle{ .x = r.x, .y = r.y, .z = r.z, .px = particle.px, .py = particle.py, .pz = particle.pz };
 }
 
 fn steps(num_steps: i32, initial_particle: Particle) Particle {
     var n: i32 = 0;
     var particle = initial_particle;
+    // As we use midpoint pushers, thus for coordinates a forward half step (+dt/2) and
+    // a backward half-step (-dt/2) are done before and after the main loop
+    particle = halfStepR(1, particle);
     while (n < num_steps) : (n += 1) {
         particle = step(particle);
     }
+    particle = halfStepR(-1, particle);
     return particle;
 }
 
 pub fn main() !void {
     print("hi!\ndoing stuff...\n", .{});
-
-    const n_particles: usize = 1_000_000;
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -62,7 +76,7 @@ pub fn main() !void {
 
     var i: usize = 0;
     while (i < n_particles) : (i += 1) {
-        const x = @intToFloat(f64, i);
+        const x = 2 * @intToFloat(f64, i) / @intToFloat(f64, n_particles - 1);
         try particles.append(Particle{ .x = x, .y = 0, .z = 0, .px = p_initial.x, .py = p_initial.y, .pz = p_initial.z });
     }
 
@@ -73,10 +87,13 @@ pub fn main() !void {
     const time2 = std.time.milliTimestamp();
     print("computation takes {d} ms\n", .{time2 - time1});
 
-    var sum: f64 = 0.0;
+    var sum_x: f64 = 0;
+    var sum_px: f64 = 0;
     for (particles.items) |particle| {
-        sum += particle.px;
+        sum_x += particle.x;
+        sum_px += particle.px;
     }
-    print("result = {e}\n", .{sum / @intToFloat(f64, n_particles)});
+    print("x = {e}\n", .{sum_x / @intToFloat(f64, n_particles)});
+    print("px = {e}\n", .{sum_px / @intToFloat(f64, n_particles)});
     print("bye!\n", .{});
 }
--- src/core.zig
+++ src/core.zig
@@ -1,20 +1,55 @@
 pub const Vec3 = struct { x: f64, y: f64, z: f64 };
 
-// Dot-product for SIMD vectors of length 3
-pub fn dot(a: @Vector(3, f64), b: @Vector(3, f64)) f64 {
-    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+// Multiply vector by a number
+pub fn mult(comptime T: type, a: f64, v: T) T {
+    return switch (T) {
+        Vec3 => Vec3{ .x = a * v.x, .y = a * v.y, .z = a * v.z },
+        // SIMD vectors of length 3
+        @Vector(3, f64) => {
+            const s = @splat(3, a);
+            s * a;
+        },
+        else => unreachable,
+    };
 }
 
-// Norm squared for SIMD vectors of length 3
-pub fn norm2(a: @Vector(3, f64)) f64 {
-    return dot(a, a);
+pub fn plus(comptime T: type, a: T, b: T) T {
+    return switch (T) {
+        Vec3 => Vec3{ .x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z },
+        @Vector(3, f64) => a + b,
+        else => unreachable,
+    };
 }
 
-// Cross-product for SIMD vectors of length 3
-pub fn cross(a: @Vector(3, f64), b: @Vector(3, f64)) @Vector(3, f64) {
-    const s = @Vector(3, f64){ a[1], a[2], a[0] };
-    const t = @Vector(3, f64){ b[2], b[0], b[1] };
-    const u = @Vector(3, f64){ a[2], a[0], a[1] };
-    const v = @Vector(3, f64){ b[1], b[2], b[0] };
-    return s * t - u * v;
+pub fn plus3(comptime T: type, a: T, b: T, c: T) T {
+    return plus(T, plus(T, a, b), c);
+}
+
+// Dot-product
+pub fn dot(comptime T: type, a: T, b: T) f64 {
+    return switch (T) {
+        Vec3 => a.x * b.x + a.y * b.y + a.z * b.z,
+        @Vector(3, f64) => a[0] * b[0] + a[1] * b[1] + a[2] * b[2],
+        else => unreachable,
+    };
+}
+
+// Norm squared
+pub fn norm2(comptime T: type, a: T) f64 {
+    return dot(T, a, a);
+}
+
+// Cross-product
+pub fn cross(comptime T: type, a: T, b: T) T {
+    return switch (T) {
+        Vec3 => Vec3{ .x = a.y * b.z - a.z * b.y, .y = a.z * b.x - a.x * b.z, .z = a.x * b.y - a.y * b.x },
+        @Vector(3, f64) => {
+            const s = @Vector(3, f64){ a[1], a[2], a[0] };
+            const t = @Vector(3, f64){ b[2], b[0], b[1] };
+            const u = @Vector(3, f64){ a[2], a[0], a[1] };
+            const v = @Vector(3, f64){ b[1], b[2], b[0] };
+            s * t - u * v;
+        },
+        else => unreachable,
+    };
 }
